b313b234010ee4ce95fa7fe95e0a898a3f0a1925
emdneto <9735060+emdneto@users.noreply.github.com>
2021-12-22 22:28:28 -0300

    feature: added otsgo installer

---
+++ otsgo/README.md
@@ -0,0 +1,40 @@
+---
+title: otsgo
+homepage: https://github.com/emdneto/otsgo
+tagline: |
+  otsgo: A simple CLI and API client for One-Time Secret written in Go.
+---
+
+
+To update or switch versions, run `webi otsgo@stable` (or `@v2.0.0`, `@beta`,
+etc).
+
+### Files
+
+These are the files / directories that are created and/or modified with this
+install:
+
+```txt
+~/.config/envman/PATH.env
+~/.local/bin/otsgo
+~/.local/opt/otsgo
+```
+
+## Cheat Sheet
+
+> `foo` doesn't exist and this text should have been replaced. It doesn't do
+> anything, but what it does is useful because it is; everybody knows it.
+
+To run foo:
+
+```bash
+ots -h
+```
+
+### Add Baz Highlighting
+
+To run foo with both bar and baz highlighting turned on:
+
+```bash
+foo --bar=baz
+```
---
+++ otsgo/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+# "'pkg_cmd_name' appears unused. Verify it or export it."
+
+function __init_ots() {
+    set -e
+    set -u
+
+    ##################
+    # Install ots #
+    ##################
+
+    # Every package should define these 6 variables
+    pkg_cmd_name="ots"
+
+    pkg_dst_cmd="$HOME/.local/bin/ots"
+    pkg_dst="$pkg_dst_cmd"
+
+    pkg_src_cmd="$HOME/.local/opt/ots-v$WEBI_VERSION/bin/ots"
+    pkg_src_dir="$HOME/.local/opt/ots-v$WEBI_VERSION"
+    pkg_src="$pkg_src_cmd"
+
+    # pkg_install must be defined by every package
+    pkg_install() {
+        # ~/.local/opt/ots-v0.99.9/bin
+        mkdir -p "$(dirname "${pkg_src_cmd}")"
+
+        # mv ./ots-*/ots ~/.local/opt/ots-v0.99.9/bin/ots
+        mv ots "${pkg_src_cmd}"
+    }
+
+    # pkg_get_current_version is recommended, but not required
+    pkg_get_current_version() {
+        # 'ots --version' has output in this format:
+        #       ots 0.99.9 (rev abcdef0123)
+        # This trims it down to just the version number:
+        #       0.99.9
+        ots version 2> /dev/null |
+            head -n 1 |
+            cut -d ' ' -f 2
+    }
+
+}
+
+__init_ots
