ad5d0f1905e93f73d1a8339996253a8f20df17f0
jojobyte <184880+jojobyte@users.noreply.github.com>
2022-02-07 16:43:35 -0700

    Remove comments

--- test/install.ps1
+++ test/install.ps1
@@ -2,9 +2,6 @@
 
 Describe "Verify webi installs correctly" {
     It "installed webi" {
-        # Test-Path resultsfile.log | Should -Be $true
-        # webi \
-        # & "$Env:USERPROFILE\.local\bin\webi-pwsh.ps1" "$exename"
         curl.exe -A "MS" https://webinstall.dev/webi | powershell
     }
 }
